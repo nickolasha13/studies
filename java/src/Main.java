3a4af0f2da3808b944cd42c21c1080f0b8103990
Bilous Mykola <nick20031103@gmail.com>
2023-09-28 14:47:30 +0300

    java studies start

---
+++ java/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    //public static void main - entry point of the program
+    // String[] args - command line arguments
+
+    public static void main(String[] args) {
+
+        // System.out.println - prints the string to the console
+
+        System.out.println("Hello world!");
+        System.out.println("Hello!");
+        // print - prints the string to the console
+        // println - prints the string to the console and adds a new line
+        // \n - new line
+        // \t - tab
+        // \b - backspace
+        // \"" - double quote
+
+        /*
+         * This is a multi-line comment
+         * This is a multi-line comment
+         */
+    }
+}
